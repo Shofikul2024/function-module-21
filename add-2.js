94d9b5884cf354e6a3f092a8fb9b96790615bddd
Shofikul2024 <islamshofi942@gmail.com>
2025-01-19 02:11:30 +0600

    added add 2

---
+++ add-2.js
@@ -0,0 +1,5 @@
+let x = 20 ;
+let y = 10 ;
+
+sum =x+y ;
+console.log( sum)
---
+++ add.js
@@ -0,0 +1,4 @@
+let applePrice=20 ;
+let bananaPrice=30 ;
+let  totalPrice =applePrice+ bananaPrice;
+console.log( totalPrice)
